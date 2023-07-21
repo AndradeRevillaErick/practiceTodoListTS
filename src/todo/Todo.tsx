17ba60a4bc1d3c43146d6ae487d46a41a0906518
Erick Andrade <erick_andradhe@hotmail.com>
2023-07-20 21:40:58 -0600

    first commit

---
+++ src/todo/Todo.tsx
@@ -0,0 +1,12 @@
+import { Tittle } from "./components/Tittle";
+import { TodoList } from "./components/TodoList";
+import { TodoProvider } from "./context/TodoProvider";
+
+export const Todo = () => {
+  return (
+    <TodoProvider>
+      <Tittle />
+      <TodoList />
+    </TodoProvider>
+  );
+};
